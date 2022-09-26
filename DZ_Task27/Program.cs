75c9235dc239ecacdd2f8cca251435352e1b4e84
alexandr <chochumakov_alek@mail.ru>
2022-09-26 22:38:03 +0300

    DZ_Task27 is ready

--- DZ_Task27/Program.cs
+++ DZ_Task27/Program.cs
@@ -2,3 +2,20 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+int SumOfFiguresInNumber(int num)
+{
+    if (num < 0) num = - num;
+    int result = 0;
+    while (num / 10 != 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    result = result + num;
+    return result;
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = SumOfFiguresInNumber(number);
+Console.WriteLine($"{number} -> {res}");
